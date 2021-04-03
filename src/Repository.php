5d86e6de20c13a4d795550d95bc87d8bf1a44537
roman <sidorovroman163@gmail.com>
2021-04-04 01:11:12 +0400

    Second

--- src/Repository.php
+++ src/Repository.php
@@ -16,7 +16,7 @@ class Repository
      */
     public function __construct()
     {
-        $this->file_path = base_path('/vendor/sidorovroman/file-site-options/fileSiteOptions.json');
+        $this->file_path = base_path('/vendor/sidorovroman/filesiteoptions/fileSiteOptions.json');
     }
 
     /**
